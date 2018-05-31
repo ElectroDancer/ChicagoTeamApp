1d6e69c83de878a1d33a77efc54120e7d5adf89a
Stas Anishchenko <anischenko_stas@mail.ru>
2018-05-31 22:40:22 +0500

    Ready 3 screens of the first row

--- app/src/main/java/com/chicagoteamapp/chicagoteamapp/LaunchActivity.java
+++ app/src/main/java/com/chicagoteamapp/chicagoteamapp/LaunchActivity.java
@@ -1,11 +1,12 @@
 package com.chicagoteamapp.chicagoteamapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends FragmentActivity {
 
     private TextView mWelcome;
     private Button mCreateAnAccount;
@@ -17,14 +18,16 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-        initUI();
+//        initUI();
     }
 
+
+    @SuppressLint("WrongViewCast")
     private void initUI() {
         mWelcome = findViewById(R.id.text_welcome);
         mCreateAnAccount = findViewById(R.id.button_create_an_account);
         mLoginWithEmail = findViewById(R.id.button_login_with_email);
-        mFacebook = findViewById(R.id.button_facebook);
+        mFacebook = findViewById(R.id.layout_facebook);
         mMoreWaysToLogin = findViewById(R.id.button_more_ways_to_login);
     }
 }
